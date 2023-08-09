cbc6533be280d7c3fdc6121619dd9183e7476022
Hyeonkyeong Park <park0600@algonquinlive.com>
2023-08-09 16:58:40 -0400

    update bg color for hamburger active

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 const hamburger = document.querySelector(".togglerIcon");
 const navMenu = document.querySelector(".list-group");
+const navbar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
+    navbar.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click"), () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-
+    navbar.classList.remove("active");
 })
 
-const hover = document.querySelector(".btn");
 
-
-$bgColor: white;
-$textHoverColor: grey;
-$debug: false;
